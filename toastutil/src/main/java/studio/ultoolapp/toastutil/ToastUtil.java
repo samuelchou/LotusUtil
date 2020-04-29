8af595311f782837543780256890f98bfcb9feee
samuelchou <samuelchou@hotmail.com.tw>
2020-04-29 21:21:11 +0800

    <Test> Managed to build a test module library(toastutil) for jCenter uploading.

---
+++ toastutil/src/main/java/studio/ultoolapp/toastutil/ToastUtil.java
@@ -0,0 +1,18 @@
+package studio.ultoolapp.toastutil;
+
+import android.content.Context;
+import android.widget.Toast;
+
+// 自定義的Toast顯示
+public class ToastUtil {
+    private static Toast mOnlyToast;
+    public static void OnlyToast(Context context, String text){
+        //此方法只會顯示一個Toast
+        if (mOnlyToast != null && mOnlyToast.getView().isShown()) {
+            mOnlyToast.cancel();
+        }
+        mOnlyToast= Toast.makeText(context,text, Toast.LENGTH_SHORT);
+        mOnlyToast.show();
+    }
+
+}
